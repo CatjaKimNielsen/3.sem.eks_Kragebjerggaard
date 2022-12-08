cef9c8dd75136149f1da6ac78fdfc7d8e1cfd2db
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-12-08 11:06:58 +0100

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+
+<footer>
+
+    <nav class="container-fluid">
+
+        <div class="row">
+
+            <div class="col-12">
+
+                ...
+
+            </div>
+
+        </div>
+
+    </nav>
+
+</footer>
---
+++ kontakt.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title> Kontakt | Kragebjerggaard </title>
+    <meta name="description" content="...">
+    <meta name="robots" content="All">
+    <meta name="author" content="...">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content=" Kragebjerggaard | Kontakt ">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="...">
+    <meta property="og:image" content="...">
+    <meta property="og:description" content="...">
+    <meta property="og:locale" content="da.DK">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+    <style> body{background-color: rgb(236, 236, 236);} </style>
+
+
+
+</head>
+
+<body>
+
+
+    <?php include "includes/navigation.php"; ?>
+
+
+    ...
+
+
+    <?php include "includes/footer.php"; ?>
+
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
