51b44121583b8aa53cdd142d37bf99809f9a244c
safaa <safaammmounir@gmail.com>
2022-12-09 02:10:46 +0100

    forsiden1

--- index.php
+++ index.php
@@ -37,6 +37,53 @@
 
     ...
 
+    <img src="images/Ko.jpeg" class="d-block w-100" alt="køer">
+
+    <br><br>
+
+<div class="row p-sm-3 p-md-5">
+<div class="col-md-6">
+    <p class="h6">
+        På kravebjerggård har vi grønne fingre, så du kan få grønsager!
+    </p>
+   <div class="card-text">
+       Vores grønsager er 100% økologiske og smager er frisk og naturlig.
+       Vi leverer til døren så du kan få den friske natur direkte til dit køkken med et par enkelte klik.
+       Vi garanterer friskhed og det kan altid smages.
+       Vi levere på land og by på alle etager så du kan slappe af og nyde naturen!
+       Tjek vores shop ud og vælg mellem masser af lækre grønsager og mejeri produkter!
+       <br><br>
+       Da vi garanterer friskhed, kan vi ikke garantere mængde, så køb nu før der er udsolgt!
+   </div>
+    <button type="button" class="btn btn-mørkebrun  col-sm-3" style="color: white" href="#">Læs mere</button>
+</div>
+    <div class="col-md-6">
+        <img src="images/carrottrail.png" class="col-md-6 col-md-3" alt="">
+    </div>
+
+</div>
+
+    <br><br>
+
+    <div class="row p-sm-3 p-md-5">
+        <div class="col-lg-6 col-md-6 col-sm-6">
+            <img src="images/farmmand.jpeg" class="20px" alt="">
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6">
+        <div class="card-text">
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </div>
+            <button type="button" class="btn btn-mørkebrun  col-sm-3" style="color: white" href="#">Læs mere</button>
+        </div>
+    </div>
+
+
+</nav>
 
     <?php include "includes/footer.php"; ?>
 
