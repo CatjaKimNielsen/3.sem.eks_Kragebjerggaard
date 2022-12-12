33dc1c39a18277c7122214e28a5bd99a1e5d451f
katarina <kata0352@edu.easj.dk>
2022-12-12 12:04:09 +0100

    forside

--- index.php
+++ index.php
@@ -43,22 +43,24 @@
 
 <div class="row p-sm-3 p-md-5">
 <div class="col-md-6">
-    <p class="h6">
+    <p class="h6 montserrat m-5">
         På kravebjerggård har vi grønne fingre, så du kan få grønsager!
     </p>
-   <div class="card-text">
+   <div class="card-text montserrat m-5">
        Vores grønsager er 100% økologiske og smager er frisk og naturlig.
-       Vi leverer til døren så du kan få den friske natur direkte til dit køkken med et par enkelte klik.
-       Vi garanterer friskhed og det kan altid smages.
-       Vi levere på land og by på alle etager så du kan slappe af og nyde naturen!
-       Tjek vores shop ud og vælg mellem masser af lækre grønsager og mejeri produkter!
+      <br> Vi leverer til døren så du kan få den friske natur direkte til dit køkken med et par enkelte klik.
+      Vi garanterer friskhed og det kan altid smages.
+       <br> Vi levere på land og by på alle etager så du kan slappe af og nyde naturen!
+       <br>Tjek vores shop ud og vælg mellem masser af lækre grønsager og mejeri produkter!
        <br><br>
        Da vi garanterer friskhed, kan vi ikke garantere mængde, så køb nu før der er udsolgt!
    </div>
-    <button type="button" class="btn btn-mørkebrun  col-sm-3" style="color: white" href="#">Læs mere</button>
+    <div class="text-center">
+    <button type="button" class="btn btn-mørkebrun btn-lg col-sm-3 m-4 montserrat" style="color: white" href="#">Læs mere</button>
+    </div>
 </div>
     <div class="col-md-6">
-        <img src="images/carrottrail.png" class="col-md-6 col-md-3" alt="">
+        <img src="images/carrottrail.png" class="col-md-6 col-md-3" alt="" id="billede">
     </div>
 
 </div>
@@ -67,10 +69,10 @@
 
     <div class="row p-sm-3 p-md-5">
         <div class="col-lg-6 col-md-6 col-sm-6">
-            <img src="images/farmmand.jpeg" class="20px" alt="">
+            <img src="images/farmmand.jpeg" id="billede" class="20px" alt="Bondemand">
         </div>
         <div class="col-lg-6 col-md-6 col-sm-6">
-        <div class="card-text">
+        <div class="card-text montserrat m-5">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
             when an unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -78,8 +80,56 @@
             It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
             and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
         </div>
-            <button type="button" class="btn btn-mørkebrun  col-sm-3" style="color: white" href="#">Læs mere</button>
+            <div class="text-center">
+                <button type="button" class="btn btn-mørkebrun btn-lg col-sm-3 m-4 montserrat" style="color: white" href="#">Læs mere</button>
+            </div>
         </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-12">
+            <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="..." class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>First slide label</h5>
+                            <p>Some representative placeholder content for the first slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Second slide label</h5>
+                            <p>Some representative placeholder content for the second slide.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="..." class="d-block w-100" alt="...">
+                        <div class="carousel-caption d-none d-md-block">
+                            <h5>Third slide label</h5>
+                            <p>Some representative placeholder content for the third slide.</p>
+                        </div>
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+
+        </div>
+
     </div>
 
 
