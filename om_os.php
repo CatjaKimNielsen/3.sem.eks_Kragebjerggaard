2c227daa5084d83d5817bb5caf4984d217cd1509
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-12-13 23:50:51 +0100

    Ryddet lidt op i koderne og tilføjet lidt 'luft'.

--- om_os.php
+++ om_os.php
@@ -21,10 +21,13 @@
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <script src="https://kit.fontawesome.com/57d07bd12e.js" crossorigin="anonymous"></script>
+
 
     <style> body{background-color: rgb(236, 236, 236);} </style>
+    <style> a{color: rgb(150, 118, 107);}  a:link{color: rgb(150, 118, 107);}  a:hover{color: rgb(150, 118, 107);}</style>
+
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Tangerine&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/57d07bd12e.js" crossorigin="anonymous"></script>
 
 
 </head>
@@ -35,64 +38,67 @@
     <?php include "includes/navigation.php"; ?>
 
 
-
-
-     <div class="Banner py-sm-4">
-    <img src="images/kragebjerggaard%20banner.png" class="py-5" alt="">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="Banner col-12 mt-5 mb-5">
+                <h1 class="tangerine text-center" style="font-size: 3.5rem">Om Kragebjerggaard</h1>
+            </div>
+        </div>
     </div>
 
 
-    <section>
-        <div class="container">
-            <div class="row py-4 py-sm-0">
-            <div class="col-md-6 p-5">
-                <h2 class="title montserrat text-center p-5">Historie</h2>
-                <p class="text montserrat">
-                    Kragebjerggaard er en fin familieejet gård med beliggenhed i flotte
-                    grønne omgivelser i Sorø. Gården blev bygget i 1961 og er blevet passet
-                    af familie igennem årene.  Bygningen står og ser ud, som den gjorde
-                    for 60 år siden, men med små justeringer og finpudsninger Konceptet
-                    med gårdbutikken og< salg af råvarer til folk i lokalområderne har
-                    altid  været der og har været vellykket for familien siden dens
-                    etablering i 1961.</p>
+    <section class="container">
+        <div class="row mt-3 mb-5">
+            <div class="col-12 col-md-6">
+                <h2 class="montserrat ms-5 mt-5 me-5" style="font-size: 1.5rem"> HISTORIE </h2>
+                <p class="montserrat ms-5 mt-3 me-5 mb-4">Kragebjerggaard er en fin familieejet gård med beliggenhed i flotte
+                    grønne omgivelser i Sorø. Gården blev bygget i 1961 og er blevet passet af familie igennem årene.
+                    Bygningen står og ser ud, som den gjorde for 60 år siden, men med små justeringer og finpudsninger Konceptet
+                    med gårdbutikken og salg af råvarer til folk i lokalområderne har altid  været der og har været vellykket for
+                    familien siden dens etablering i 1961.</p>
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="gaardbutikken.php"> Gårdbutikken </a>
             </div>
-            <div class="col-md-6 p-5">
-                <img src="images/Gården.jpg" class="img-fluid" alt="">
-            </div>
-                <div class="col-md-6 p-5">
-                    <h2 class="title montserrat text-center p-5" >Dyrevelfærd</h2>
-                    <p class="text montserrat">
-                        På Kragebjerggaard går vi meget op i vores dyrs trivsel og at de behandles
-                        godt  og lever under gode forhold.Vi arbejder dagligt på at dyrenes
-                        fysiologiske sundhedsmæssige  og adfærdsmæssige behov opfyldes så bedst som muligt.
-                        Dyrene er ude om sommeren og kan også klare at være ude om vinteren,
-                        men det kan blive svært ift. fodring udenfor om vinteren, og derfor
-                        bringes de ind på standene som har et stort areal hvor de går frit rundt
-                        på lag af dybtstrøelse der udskiftes dagligt.
-                    </p>
-                </div>
-            <div class="col-md-6 p-5">
-                    <img src="images/Dyrevel.jpg" class="img-fluid"alt="">
-                </div>
-
-            <div class="col-md-6 p-5">
-                    <h2 class="title montserrat text-center p-5">Værdigrundlag</h2>
-                    <p class="text montserrat">
-                        Alle vores produkter er økologiske og dyrket i Danmark. Vi har stor fokus på
-                        at bevare og passe på naturen. Vi anvender kun sprøjtemidler og jordforbedringsmidler
-                        som er godkendt til økologisk dyrkning, dvs. at sprøjterne kun indeholder stoffer
-                        der  kan findes i naturen, i planter og i fødevarer. De naturlige pesticider vi
-                        anvender bruges til  at bekæmpe og forebygge b.la. skadedyr og svampeangreb.
-                    </p>
-                </div>
-                <div class="col-md-6 p-5">
-                    <img src="images/Økologi.jpg" class="img-fluid" alt="">
-                </div>
+            <div class="col-12 col-md-6">
+                <img src="images/Gården.jpg" class="mb-5 mb-md-0" alt="Kragebjerggaard">
             </div>
         </div>
     </section>
 
 
+    <section class="container">
+        <div class="row mt-5 mb-5">
+            <div class="col-12 col-md-6">
+                <h2 class="montserrat ms-5 mt-5 me-5" style="font-size: 1.5rem"> DYREVELFÆRD </h2>
+                <p class="montserrat ms-5 mt-3 me-5 mb-4">På Kragebjerggaard går vi meget op i vores dyrs trivsel og at de behandles
+                    godt og lever under gode forhold.Vi arbejder dagligt på at dyrenes fysiologiske sundhedsmæssige  og adfærdsmæssige
+                    behov opfyldes så bedst som muligt. Dyrene er ude om sommeren og kan også klare at være ude om vinteren, men det kan
+                    blive svært ift. fodring udenfor om vinteren, og derfor bringes de ind på standene som har et stort areal hvor de går
+                    frit rundt på lag af dybtstrøelse der udskiftes dagligt.</p>
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="gaardbutikken.php"> Bestil varer </a>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="images/Dyrevel.jpg" class="mb-5 mb-md-0" alt="Kragebjerggaards får i stalden">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="container">
+        <div class="row mt-5 mb-5">
+            <div class="col-12 col-md-6">
+                <h2 class="montserrat ms-5 mt-5 me-5" style="font-size: 1.5rem"> VÆRDIGRUNDLAG </h2>
+                <p class="montserrat ms-5 mt-3 me-5 mb-4">Alle vores produkter er økologiske og dyrket i Danmark. Vi har stor fokus på
+                    at bevare og passe på naturen. Vi anvender kun sprøjtemidler og jordforbedringsmidler som er godkendt til
+                    økologisk dyrkning, dvs. at sprøjterne kun indeholder stoffer der kan findes i naturen, i planter og i
+                    fødevarer. De naturlige pesticider vi anvender bruges til  at bekæmpe og forebygge b.la. skadedyr og
+                    svampeangreb.</p>
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="gaardbutikken.php"> Kontakt </a>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="images/Økologi.jpg" class="mb-5 mb-md-0" alt="Kragebjerggaards værdier">
+            </div>
+        </div>
+    </section>
 
 
     <?php include "includes/footer.php"; ?>
