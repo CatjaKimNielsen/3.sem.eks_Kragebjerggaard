f6773052d280864041f743b25c63b8054fdbe714
Helene <Helenecolberglaursen@hotmail.dk>
2022-12-13 11:48:32 +0100

    footer ting

--- includes/footer.php
+++ includes/footer.php
@@ -28,36 +28,37 @@
 
         <div class="row ps-4 ps-md-5 pt-5 pe-4 pe-md-5 pb-5 bg-mørkegrøn">
 
-            <div class="col-12 col-md-4 mb-5">
+            <div class="col-12 col-lg-4 mb-5 px-5">
+
                 <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> KRAVEBJERGGAARD </h3>
                 <p class="montserrat"> Kravebjergaardvej 7, 4296 Sorø <br><br> Tlf.: +45 71 72 73 74 <br> E-mail: kontakt@kragebjerggaard.dk</p>
             </div>
 
-            <div class="col-12 col-md-6 mb-5">
-                <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem">GÅRDBUTIKKENS ÅBNINGSTIDER </h3>
-                <div class="row">
-                    <div class="col-4 col-md-2">
-                        <p class="montserrat">Mandag:</p>
-                        <p class="montserrat">Tirsdag:</p>
-                        <p class="montserrat">Onsdag:</p>
-                        <p class="montserrat">Torsdag:</p>
-                        <p class="montserrat">Fredag:</p>
-                        <p class="montserrat">Lørdag:</p>
-                        <p class="montserrat">Søndag:</p>
+            <div class="col-12 col-lg-4 mb-5">
+                <h3 class="montserrat mt-5 mb-4 text-center" style="font-size: 1.5rem">GÅRDBUTIKKENS <br>ÅBNINGSTIDER </h3>
+                <div class="row montserrat">
+                    <div class="col-6  text-end">
+                        <p>Mandag:</p>
+                        <p>Tirsdag:</p>
+                        <p>Onsdag:</p>
+                        <p>Torsdag:</p>
+                        <p>Fredag:</p>
+                        <p>Lørdag:</p>
+                        <p>Søndag:</p>
                     </div>
-                    <div class="col-8 col-md-10">
-                        <p class="montserrat">09:00-17.00</p>
-                        <p class="montserrat">09:00-17.00</p>
-                        <p class="montserrat">Lukket</p>
-                        <p class="montserrat">09:00-17.00</p>
-                        <p class="montserrat">09:00-14.00</p>
-                        <p class="montserrat">09:00-17.00</p>
-                        <p class="montserrat">Lukket</p>
+                    <div class="col-6">
+                        <p>09:00-17.00</p>
+                        <p>09:00-17.00</p>
+                        <p>Lukket</p>
+                        <p>09:00-17.00</p>
+                        <p>09:00-14.00</p>
+                        <p">09:00-17.00</p>
+                        <p>Lukket</p>
                     </div>
                 </div>
             </div>
 
-            <div class="col-12 col-md-2 mb-5">
+            <div class="col-12 col-lg-4 mb-5 text-center">
                 <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> FØLG OS </h3>
 
                 <br>
