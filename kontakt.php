1ff21f8064487eeb91b566f4e87f27de342ac5e8
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-12-13 20:17:55 +0100

    Småting rettet til og tilføjet 'luft'.

--- kontakt.php
+++ kontakt.php
@@ -21,10 +21,13 @@
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <script src="https://kit.fontawesome.com/57d07bd12e.js" crossorigin="anonymous"></script>
+
 
     <style> body{background-color: rgb(236, 236, 236);} </style>
+    <style> a{color: rgb(150, 118, 107);}  a:link{color: rgb(150, 118, 107);}  a:hover{color: rgb(150, 118, 107);}</style>
+
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Tangerine&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/57d07bd12e.js" crossorigin="anonymous"></script>
 
 
 </head>
@@ -35,50 +38,59 @@
     <?php include "includes/navigation.php"; ?>
 
 
-    <div class="row p-sm-3 p-md-5">
-        <div class="col-sm-12 col-md-6">
-            <div class="mb-3">
-                <h2 class="my-5 montserrat">Kontaktformular</h2>
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="Navn" style="width: 75%">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="Banner col-12 mt-5 mb-5">
+                <h1 class="tangerine text-center" style="font-size: 3.5rem"> Kontakt os </h1>
+                <p class="montserrat text-center"> Vi bestræer os på, at svare indenfor 24 timer.</p>
             </div>
-            <div class="mb-3">
-                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="navn@eksempel.dk" style="width: 75%">
-            </div>
-            <div class="mb-3">
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" placeholder="Skriv din tekst her"></textarea>
-            </div>
-            <button type="button" class="btn btn-mørkebrun btn-lg montserrat" data-bs-toggle="button" style="color: white" href="#">Send Email</button>
         </div>
-
-        <div class="col-sm-12 col-md-6 ml-2 mt-5">
-            <img src="images/gaardbutik.jpg" id="billede" alt="Gårdbutik og ejere">
-        </div>
-
     </div>
 
 
-    <div class="row">
-        <div class="col-12 text-center">
-            <h2 class="montserrat">Læs også:</h2>
+    <div class="container">
+        <div class="row mt-3 mb-5">
+            <div class="col-12 col-md-6">
+                <div class="row">
+                    <div class="col-12 col-md-9 mb-3">
+                        <input type="email" class="form-control montserrat" id="exampleFormControlInput1" placeholder="Navn">
+                    </div>
+                    <div class="col-12 col-md-9 mb-3">
+                        <input type="email" class="form-control montserrat" id="exampleFormControlInput1" placeholder="navn@eksempel.dk">
+                    </div>
+                    <div class="col-12 mb-3">
+                        <textarea class="form-control montserrat" id="exampleFormControlTextarea1" rows="8" placeholder="Skriv din tekst her"></textarea>
+                    </div>
+                    <div class="col-12 mt-3 mb-3 d-flex justify-content-end">
+                        <a type="button" class="btn btn-mørkebrun montserrat text-white" data-bs-toggle="button" style="width: 200px" href="shop.php"> Send </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="images/gaardbutik.jpg" id="billede" alt="Gårdbutik og ejere">
+            </div>
         </div>
     </div>
 
-    <div class="row p-sm-3 p-md-5">
-        <div class="col-sm-6 col-md-4 text-center">
-            <img src="images/Gården.jpg" id="Gård" alt="Gård" class="mt-4">
-            <button type="button" class="btn btn-mørkebrun btn-lg m-4 montserrat" style="color: white" href="#">Læs om historien</button>
-        </div>
 
-        <div class="col-sm-6 col-md-4 text-center">
-            <img src="images/koer.jpeg" id="Koer" alt="Køer" class="mt-4">
-            <button type="button" class="btn btn-mørkebrun btn-lg m-4 montserrat" style="color: white" href="#">Læs om køerne</button>
+    <div class="container">
+        <div class="row mt-5 mb-5">
+            <div class="col-12 text-center">
+                <h2 class="montserrat ms-5 mt-5 me-5" style="font-size: 1.5rem"> LÆS MERE OM KRAGEBJERGGAARD </h2>
+            </div>
+            <div class="col-12 col-md-4 text-center mt-5 mb-5">
+                <img src="images/Gården.jpg" alt="Gård">
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="kontakt.php"> Om gården </a>
+            </div>
+            <div class="col-12 col-md-4 text-center mt-5 mb-5">
+                <img src="images/mark3.jpeg" alt="Gård">
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="kontakt.php"> Shop online </a>
+            </div>
+            <div class="col-12 col-md-4 text-center mt-5 mb-5">
+                <img src="images/farmmand.jpeg" alt="Gård">
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="kontakt.php"> Gårdbutikken </a>
+            </div>
         </div>
-
-        <div class="col-sm-6 col-md-4 text-center">
-            <img src="images/Gården.jpg" id="Gård" alt="Gård" class="mt-4">
-            <button type="button" class="btn btn-mørkebrun btn-lg m-4 montserrat" style="color: white" href="#">Læs om gården</button>
-        </div>
-
     </div>
 
 
