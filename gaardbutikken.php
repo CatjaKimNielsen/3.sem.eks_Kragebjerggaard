f9ae56037fa65c981771b7f66e26b217f134c345
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-12-13 17:35:58 +0100

    Siden 'gårdbutikken' er kodet færdig.

--- gaardbutikken.php
+++ gaardbutikken.php
@@ -8,7 +8,7 @@
     <title> Gårdbutikken | Kragebjerggaard </title>
     <meta name="description" content="...">
     <meta name="robots" content="All">
-    <meta name="author" content="...">
+    <meta name="author" content="Catja, Katarina, Helene, Aisha og Safa">
     <meta name="copyright" content="Information om copyright">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -21,10 +21,13 @@
 
     <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
     <link href="css/styles.css" rel="stylesheet" type="text/css">
-    <script src="https://kit.fontawesome.com/57d07bd12e.js" crossorigin="anonymous"></script>
+
 
     <style> body{background-color: rgb(236, 236, 236);} </style>
+    <style> a{color: rgb(150, 118, 107);}  a:link{color: rgb(150, 118, 107);}  a:hover{color: rgb(150, 118, 107);}</style>
+
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Tangerine&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/57d07bd12e.js" crossorigin="anonymous"></script>
 
 
 </head>
@@ -35,7 +38,116 @@
     <?php include "includes/navigation.php"; ?>
 
 
-    ...
+    <div class="container-fluid">
+        <div class="row">
+            <div class="Banner col-12 mt-5 mb-5">
+                <h1 class="tangerine text-center" style="font-size: 3.5rem">Kragebjerggaard Gårdbutik</h1>
+            </div>
+        </div>
+    </div>
+
+
+    <section class="container">
+        <div class="row mt-3 mb-5">
+            <div class="col-12 col-md-6">
+                <h2 class="montserrat ms-5 mt-5 me-5" style="font-size: 1.5rem"> ØKOLOGISK GÅRDBUTIK </h2>
+                <p class="montserrat ms-5 mt-3 me-5 mb-4">Vores gårdbutik har placering ved siden af vores gård i Sorø, hvor vi altid har
+                    hylderne fyldt med friske råvarer fra egen produktion. Vores medarbejdere står klar til at hjælpe dig, og give dig
+                    inspiration til lækre retter med sæsonens frugter og grøntsager, samt vores mælkeprodukter.
+                    <br><br>
+                    Gårdbutikkens sortiment er altid præget af den igangværende sæson, så du ved at alt det du køber hos os kommer
+                    direkte fra gården.</p>
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="shop.php"> Shop nu </a>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="images/gaardbutik.jpg" class="mb-5 mb-md-0" alt="Kragebjerggaards ejere i gårdbutikken">
+            </div>
+        </div>
+    </section>
+
+
+    <section class="container">
+        <div class="row mt-5 mb-5 pt-5 pb-2">
+            <div class="col-12">
+                <h2 class="montserrat text-center mb-5" style="font-size: 1.5rem"> MÆLK OG GRØNTSAGER FRA EGNE KØER OG MARKER! </h2>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="images/koer.jpeg" class="mb-5 mb-md-0" alt="Kragebjerggaards køer på græs">
+            </div>
+            <div class="col-12 col-md-6">
+                <p class="montserrat ms-5 mt-3 me-5 mb-5">På vores gård har vi 167 køer, der alle sammen fortjener det bedste liv.
+                    Fordi, ikke nok med at økologi er vigtigt, betyder dyrevelfærd også meget for os. Derfor har vi dedikeret et stort
+                    areal af vores gård til at køerne frit kan græsse i de varme måneder. Når vejret bliver koldt har vi en stor varm
+                    kostald, hvor køerne får økologisk fodder og har massere af plads at gå rundt og hygge sig på.
+                    <br><br>
+                    Alle vores marker er økologiske, og det vi producerer altid i sæson. Det giver ikke kun en bedre høst, men også en
+                    langt bedre smag i råvarerne. Vi tror på at de bedste råvarer kommer når man arbejder sammen med naturen, så derfor
+                    vil du ofte kunne se en af vores mange gårdkatte løbe rundt på markerne.</p>
+            </div>
+        </div>
+    </section>
+
+
+    <div class="container">
+        <div class="row mb-5">
+            <div class="col-12 col-md-6">
+                <div class="row">
+                    <div class="col-6 mb-3">
+                        <img src="images/mark1.jpeg" alt="Kragebjerggaards afgrøder på marken">
+                    </div>
+                    <div class="col-6 mb-3">
+                        <img src="images/mark2.jpeg" alt="Sol over Kragebjerggaards marker">
+                    </div>
+                    <div class="col-6 mb-3">
+                        <img src="images/koer2.jpeg" alt="Sol over Kragebjerggaards marker">
+                    </div>
+                    <div class="col-6 mb-3">
+                        <img src="images/koer3.jpeg" alt="Sol over Kragebjerggaards marker">
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <img src="images/mark3.jpeg" alt="Kragbjergaards grøntsager er blevet gravet op">
+            </div>
+        </div>
+    </div>
+
+
+    <section class="container-fluid bg-lysebrun">
+        <div class="row mt-5 mb-5">
+            <div class="col-12">
+                <h2 class="montserrat ms-5 mt-5 me-5 text-center" style="font-size: 1.5rem"> GÅRDBUTIKKENS ÅBNINGSTIDER </h2>
+                <div class="montserrat ms-5 mt-4 me-5 mb-5 text-center">
+                    <p>Mandag: 09.00 - 17.00</p>
+                    <p>Tirsdag: 09.00 - 17.00</p>
+                    <p>Onsdag: lukket</p>
+                    <p>Torsdag 09.00 - 17.00</p>
+                    <p>Fredag 09.00 - 17.00</p>
+                    <p>Lørdag: 09.00 - 17.00</p>
+                    <p>Søndag: lukket</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <nav class="container">
+        <div class="row mt-5 mb-5">
+            <div class="col-12 col-md-7">
+                <h2 class="montserrat ms-5 mt-5 me-5" style="font-size: 1.5rem"> HISTORIE, DYREVELFÆRD OG VÆRDIER </h2>
+                <p class="montserrat ms-5 mt-3 me-5 mb-5">Kragebjerggaard er en familiegård der har været gennem mange genrationer i
+                    familiens stamtræ. Gennem alle årene og alle generationerne har vi udviklet os utrolig meget, men 2 ting er sikre;
+                    vi går ikke på kompromis med dyrevelfærden og vores værdigrundlag.
+                    <br><br>
+                    Er du blevet nysgerrig og gerne vil vide mere kan du læse mere
+                    <a href="om_os.php">om gården</a>, komme forbi gårdbutikken til en rudnvisning/fortælling eller kontakte os. </p>
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="kontakt.php"> Kontakt os </a>
+            </div>
+            <div class="col-12 col-md-5 mt-5 mt-md-0">
+                <img src="images/milktrail.png" class="mb-5" alt="Kragebjerggaards ejere i gårdbutikken">
+            </div>
+        </div>
+    </nav>
 
 
     <?php include "includes/footer.php"; ?>
