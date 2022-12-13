4154285f04e066d72737150204ee19d912250d7b
Aisha <AishaaMalik@hotmail.com>
2022-12-13 10:02:09 +0100

    Banner flyttet ned

--- om_os.php
+++ om_os.php
@@ -34,7 +34,11 @@
     <?php include "includes/navigation.php"; ?>
 
 
-        <img src="images/kragebjerggaard%20banner.png"alt="">
+
+    <img src="images/kragebjerggaard%20banner.png" class="py-5" alt="">
+
+
+
 
     <section>
         <div class="container py-5">
