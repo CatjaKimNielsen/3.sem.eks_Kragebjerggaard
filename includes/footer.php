ae2342566987cf348016d65864a9a1ab98651272
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-12-13 16:19:27 +0100

    Kravebjerggaard er rettet til Kragebjerggaard. Og delen med nyhedsbrevet er blevet gjort responsivt!

--- includes/footer.php
+++ includes/footer.php
@@ -1,73 +1,58 @@
-<?php
-
-
-
-?>
-
-
 
 
 <footer class="text-white ">
-    <div class="row bg-lysegrøn p-4 text-center">
-        <div class="col-6">
-            <h5 class="montserrat">TILMELD VORES NYHEDSBREV OG FÅ NYHEDER DIREKTE I DIN INDBAKKE</h5>
-        </div>
 
-        <div class="col-4">
+    <div class="row bg-lysegrøn p-4 text-center">
+        <div class="col-12 col-md-6">
+            <p class="montserrat mb-3">Tilmeld vores NYHEDSBREV og få nyheder direkte i din indbakke</p>
+        </div>
+        <div class="col-12 col-md-4">
             <div class="mb-3">
-                <input type="email" class="form-control mx-2" id="exampleFormControlInput1" placeholder="Din e-mail" style="width:100%">
+                <input type="email" class="form-control montserrat mx-2" id="exampleFormControlInput1" placeholder="Din e-mail" style="width:100%">
             </div>
         </div>
-
-        <div class="col-2">
-            <button type="button" class="btn btn-lysebrun montserrat px-lg-5 px-sm-4 mx-2" data-bs-toggle="button" style="color: white" href="#">Tilmeld</button>
+        <div class="col-12 col-md-2">
+            <a type="button" class="btn btn-lysebrun montserrat text-white" data-bs-toggle="button" style="width: 200px" href="#">Tilmeld</a>
         </div>
-
     </div>
 
-
-        <div class="row ps-4 ps-md-5 pt-5 pe-4 pe-md-5 pb-5 bg-mørkegrøn">
-
-            <div class="col-12 col-lg-4 mb-5 px-5">
-
-                <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> KRAVEBJERGGAARD </h3>
-                <p class="montserrat"> Kravebjergaardvej 7, 4296 Sorø <br><br> Tlf.: +45 71 72 73 74 <br> E-mail: kontakt@kragebjerggaard.dk</p>
-            </div>
-
-            <div class="col-12 col-lg-4 mb-5">
-                <h3 class="montserrat mt-5 mb-4 text-center" style="font-size: 1.5rem">GÅRDBUTIKKENS <br>ÅBNINGSTIDER </h3>
-                <div class="row montserrat">
-                    <div class="col-6  text-end">
-                        <p>Mandag:</p>
-                        <p>Tirsdag:</p>
-                        <p>Onsdag:</p>
-                        <p>Torsdag:</p>
-                        <p>Fredag:</p>
-                        <p>Lørdag:</p>
-                        <p>Søndag:</p>
-                    </div>
-                    <div class="col-6">
-                        <p>09:00-17.00</p>
-                        <p>09:00-17.00</p>
-                        <p>Lukket</p>
-                        <p>09:00-17.00</p>
-                        <p>09:00-14.00</p>
-                        <p">09:00-17.00</p>
-                        <p>Lukket</p>
-                    </div>
+    <div class="row ps-4 ps-md-5 pt-5 pe-4 pe-md-5 pb-5 bg-mørkegrøn">
+        <div class="col-12 col-lg-4 mb-5 px-5">
+            <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> KRAGEBJERGGAARD </h3>
+            <p class="montserrat"> Kragebjergaardvej 7, 4296 Sorø <br><br> Tlf.: +45 71 72 73 74 <br> E-mail: kontakt@kragebjerggaard.dk</p>
+        </div>
+        <div class="col-12 col-lg-4 mb-5">
+            <h3 class="montserrat mt-5 mb-4 text-center" style="font-size: 1.5rem">GÅRDBUTIKKENS <br>ÅBNINGSTIDER </h3>
+            <div class="row montserrat">
+                <div class="col-6  text-end">
+                    <p>Mandag</p>
+                    <p>Tirsdag</p>
+                    <p>Onsdag</p>
+                    <p>Torsdag</p>
+                    <p>Fredag</p>
+                    <p>Lørdag</p>
+                    <p>Søndag</p>
+                </div>
+                <div class="col-6">
+                    <p>09:00-17.00</p>
+                    <p>09:00-17.00</p>
+                    <p>Lukket</p>
+                    <p>09:00-17.00</p>
+                    <p>09:00-14.00</p>
+                    <p">09:00-17.00</p>
+                    <p>Lukket</p>
                 </div>
             </div>
-
-            <div class="col-12 col-lg-4 mb-5 text-center">
-                <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> FØLG OS </h3>
-
-                <br>
-                <div>
-                    <i class="fab fa-instagram fa-3x p-1"></i>
-                    <i class="fab fa-facebook-square fa-3x p-1"></i>
-                    <i class="fab fa-facebook-messenger fa-3x p-1"></i>
-                </div>
+        </div>
+        <div class="col-12 col-lg-4 mb-5 text-center">
+            <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> FØLG OS </h3>
+            <div>
+                <i class="fab fa-instagram fa-3x p-1"></i>
+                <i class="fab fa-facebook-square fa-3x p-1"></i>
+                <i class="fab fa-facebook-messenger fa-3x p-1"></i>
             </div>
+        </div>
+
 </footer>
 
 
