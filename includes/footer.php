6b46980a5f86d7208c62521e50a2cfcf0a0e1b72
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-12-13 09:01:39 +0100

    Lidt ekstra tilføjet :-D

--- includes/footer.php
+++ includes/footer.php
@@ -2,17 +2,17 @@
 
 <footer class="text-white bg-mørkegrøn">
 
-        <div class="row ps-md-5 pt-5 pe-md-5 pb-5">
+        <div class="row ps-4 ps-md-5 pt-5 pe-4 pe-md-5 pb-5">
 
-            <div class="col-12 col-md-4">
-                <h3 class="montserrat mb-4" style="font-size: 1.5rem"> KRAVEBJERGGAARD </h3>
+            <div class="col-12 col-md-4 mb-5">
+                <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> KRAVEBJERGGAARD </h3>
                 <p class="montserrat"> Kravebjergaardvej 7, 4296 Sorø <br><br> Tlf.: +45 71 72 73 74 <br> E-mail: kontakt@kragebjerggaard.dk</p>
             </div>
 
-            <div class="col-12 col-md-6">
-                <h3 class="montserrat mb-4" style="font-size: 1.5rem">GÅRDBUTIKKENS ÅBNINGSTIDER </h3>
+            <div class="col-12 col-md-6 mb-5">
+                <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem">GÅRDBUTIKKENS ÅBNINGSTIDER </h3>
                 <div class="row">
-                    <div class="col-6 col-md-2">
+                    <div class="col-4 col-md-2">
                         <p class="montserrat">Mandag:</p>
                         <p class="montserrat">Tirsdag:</p>
                         <p class="montserrat">Onsdag:</p>
@@ -21,7 +21,7 @@
                         <p class="montserrat">Lørdag:</p>
                         <p class="montserrat">Søndag:</p>
                     </div>
-                    <div class="col-6 col-md-10">
+                    <div class="col-8 col-md-10">
                         <p class="montserrat">09:00-17.00</p>
                         <p class="montserrat">09:00-17.00</p>
                         <p class="montserrat">Lukket</p>
@@ -33,8 +33,8 @@
                 </div>
             </div>
 
-            <div class="col-12 col-md-2">
-                <h3 class="montserrat mb-4" style="font-size: 1.5rem"> FØLG OS </h3>
+            <div class="col-12 col-md-2 mb-5">
+                <h3 class="montserrat mt-5 mb-4" style="font-size: 1.5rem"> FØLG OS </h3>
                 <div>
                     <i fa-regular fa-insta> Facebook </i> <br>
                     <i fa-regular fa-fb> Instagram </i> <br>
