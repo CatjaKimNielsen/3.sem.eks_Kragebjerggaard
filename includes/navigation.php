574ba7998159dbf9b4ff853c06efec9447a2e4f5
catj0073@edu.zealand.dk <ZeaZEYVH!2>
2022-12-13 14:43:02 +0100

    Kravebjerggaard er rettet til Kragebjerggaard.

--- includes/navigation.php
+++ includes/navigation.php
@@ -2,8 +2,9 @@
 <nav class="navbar navbar-expand-lg navbar-dark position-fixed w-100 py-0" id="nav_bar">
 
     <div class="container-fluid bg-mørkegrøn ps-md-5 pt-2 pe-md-5 pb-2">
-        <a class="navbar-brand" href="index.php"> <img id="logo" src="images/coclologo.png" ></a>
-        <a class="navbar-brand tangerine m-2 h3" aria-current="page" style="color: white"  href="index.php"><p class="h1">Kravebjerggaard</p></a>
+        <a class="navbar-brand" href="index.php"> <img id="logo" src="images/coclologo.png"> </a>
+        <a class="navbar-brand tangerine m-2" aria-current="page" style="color: white"  href="index.php"><p class="h1">Kragebjerggaard</p></a>
+
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -11,16 +12,16 @@
         <div class="collapse navbar-collapse w-100 justify-content-end" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link montserrat m-2" aria-current="page" style="color: white" href="shop.php"><h5>Onlineshop</h5></a>
+                    <a class="nav-link montserrat m-2" aria-current="page" style="color: white" href="shop.php"><p>Onlineshop</p></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link montserrat m-2" aria-current="page" style="color: white" href="gaardbutikken.php"><h5>Gårdbutikken</h5></a>
+                    <a class="nav-link montserrat m-2" aria-current="page" style="color: white" href="gaardbutikken.php"><p>Gårdbutikken</p></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link montserrat m-2" aria-current="page" style="color: white" href="om_os.php"><h5>Om os</h5></a>
+                    <a class="nav-link montserrat m-2" aria-current="page" style="color: white" href="om_os.php"><p>Om os</p></a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link montserrat m-2" aria-current="page"style="color: white"  href="kontakt.php"><h5>Kontakt</h5></a>
+                    <a class="nav-link montserrat m-2" aria-current="page" style="color: white"  href="kontakt.php"><p>Kontakt</p></a>
                 </li>
                 <li class="nav-item mx-3">
                     <a class="nav-link" aria-current="page" style="color: white" href="#">
