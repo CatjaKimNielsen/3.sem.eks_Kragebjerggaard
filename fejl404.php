52d351fb16fbddda948993372cd2d3024be56eea
Helene <Helenecolberglaursen@hotmail.dk>
2022-12-15 12:40:48 +0100

    fejlside started

---
+++ fejl404.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html lang="da">
+
+<head>
+
+    <meta charset="utf-8">
+
+    <title> Kragebjerggaard | Fejl404 </title>
+    <meta name="description" content="Hos Kragebjerggaard sælger vi kun økologiske grøntsager og mælkeprodukter fra egne marker, dyr
+    og mejeri. De økologiske gårdvarer kan købes i vores gårdbutik og shoppes online med levering til døren.">
+    <meta name="robots" content="All">
+    <meta name="author" content="Catja, Katarina, Helene, Aisha og Safaa">
+    <meta name="copyright" content="Information om copyright">
+    <meta name="keywords" content="Kragebjerggård, økologiske grøntsager, økologiske grøntsager til døren, mejeri, økologisk mælk, gårdbutik">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta property="og:title" content=" Kragebjerggaard | Økologisk gård ">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="...">
+    <meta property="og:image" content="...">
+    <meta property="og:description" content="Hos Kragebjerggaard sælger vi kun økologiske grøntsager og mælkeprodukter fra egne marker,
+    dyr og mejeri. De økologiske gårdvarer kan købes i vores gårdbutik og shoppes online med levering til døren.">
+    <meta property="og:locale" content="da.DK">
+
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/styles.css" rel="stylesheet" type="text/css">
+
+
+    <style> body{background-color: rgb(236, 236, 236);} </style>
+    <style> a{color: rgb(150, 118, 107);}  a:link{color: rgb(150, 118, 107);}  a:hover{color: rgb(150, 118, 107);}</style>
+
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&family=Tangerine&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/57d07bd12e.js" crossorigin="anonymous"></script>
+
+    <link rel="shortcut icon" href="images/coclologo.png" type="image/x-icon" alt="Logo">
+</head>
+
+<body>
+
+
+    <?php include "includes/navigation.php"; ?>
+<div style="padding-top: 93px">
+
+
+    <section class="container p-5 text-center align-items-center">
+        <div class="row p-5 m-5">
+            <nav class="col-12">
+                <h1 class="montserrat ms-5 mt-5 me-5 fw-bold" style="font-size: 1.5rem"> Hov!</h1>
+                <p class="montserrat ms-5 mt-3 me-5 mb-3 fw-bold">Det ser ud til at der er en ko på isen! <br>
+                    Klik her den for at vende tilbage til forsiden:
+                </p>
+                <a type="button" class="btn btn-mørkebrun montserrat ms-5 mt-3 me-5 mb-5 text-white" data-bs-toggle="button" style="width: 200px" href="index.php"> Vend tilbage til forsiden </a>
+            </nav>
+            <div class="col-12 p-5 justify-content-center">
+                <img src="images/kois.png" class="m-5 p-5" alt="Ko på is">
+            </div>
+
+        </div>
+    </section>
+
+
+
+
+
+
+    <?php include "includes/footer.php"; ?>
+
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+
+
+</body>
+
+</html>
