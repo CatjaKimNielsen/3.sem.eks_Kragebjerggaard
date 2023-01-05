13718148e6790aed7d08a84488b0375510e58af4
katarina <kata0352@edu.easj.dk>
2023-01-05 11:39:57 +0100

    ekstern logo

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,4 @@
-
-
-<footer class="text-white mt-5">
+<footer class="text-white mt-5" xmlns="http://www.w3.org/1999/html">
 
     <div class="row bg-lysegrøn p-4 text-center">
         <div class="col-12 col-md-6">
@@ -16,6 +14,17 @@
         </div>
     </div>
 
+    <div class="row ps-4 ps-md-5 pt-5 pe-4 pe-md-5 pb-5 bg-mørkegrøn">
+        <div class="col-12 col-lg-4">
+            <a href="https://www.foedevarestyrelsen.dk/Leksikon/Sider/EU_okologilogo.aspx" > <img src="images/eu_blad.png" alt="EU's grønne blad" id="ekslogo"> </a>
+        </div>
+        <div class="col-12 col-lg-4">
+            <a href="https://www.foedevarestyrelsen.dk/Leksikon/Sider/%C3%98-m%C3%A6rket.aspx"> <img src="images/Logo_Statskontrolleret%20Økologisk_Sort_JPG.jpg" alt="Statensøkologi" id="ekslogo"> </a>
+        </div>
+        <div class="col-12 col-lg-4">
+            <a href="https://www.findsmiley.dk/Sider/Forside.aspx"> <img src="images/mainSmiley.png"  alt="Smiley rapport" id="ekslogo"> </a>
+        </div>
+    </div>
     <div class="row ps-4 ps-md-5 pt-5 pe-4 pe-md-5 pb-5 bg-mørkegrøn">
         <div class="col-12 col-lg-4 mb-5 px-5">
             <h3 class="montserrat mt-5 mb-4 text-center" style="font-size: 1.5rem"> KRAGEBJERGGAARD </h3>
